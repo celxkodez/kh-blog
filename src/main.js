55a87a0228b47573118fdd4b6b2a33176e79dae0
celxkodez <decele2011@gmail.com>
2020-09-22 16:16:16 +0100

    basic components created

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+// import App from './App'
+import Basic from './components/Basic'
+import $ from 'jquery'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+// import Header from './components/fractions/Header'
+// import footer from './components/fractions/Footer'
+import {
+  router
+} from './router'
+console.log($)
 
 Vue.config.productionTip = false
 
@@ -10,6 +19,11 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  render: h => h(Basic)
+  // components: {
+  // footer,
+  // Header
+  //   App
+  // }
+  // template: '<App/>'
 })
