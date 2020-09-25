e4da28f8f03591f113ea14dc5416ca012ad15e06
celxkodez <decele2011@gmail.com>
2020-09-25 17:50:56 +0100

    some

---
+++ src/components/pages/NewPost.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-sm-6">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p
+              class="card-text"
+            >With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p
+              class="card-text"
+            >With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" class="btn btn-primary">View Post</a>
+            <a href="#" class="btn btn-success">
+              <i class="fa fa-pencil" aria-hidden="true"></i>
+              Edit
+            </a>
+            <a href="#" class="btn btn-danger">
+              <i class="fa fa-trash" aria-hidden="true"></i>
+              Delete
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'New_Posts'
+}
+</script>
+
+<style>
+
+</style>
